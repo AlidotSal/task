166c4afce86fc1517ab5fed5ba0e13a44faff28c
Ali Sal <ali.mekanik7@gmail.com>
2024-11-27 18:16:01 +0330

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useLaunchParams, miniApp, useSignal } from '@telegram-apps/sdk-react';
 
-import { AppRoot, Text } from '@telegram-apps/telegram-ui';
+import { AppRoot, Section, Text } from '@telegram-apps/telegram-ui';
 
 import './App.css';
 
@@ -13,6 +13,14 @@ const App = () => {
       platform={['macos', 'ios'].includes(lp.platform) ? 'ios' : 'base'}
     >
       <Text className="text-black dark:text-white">some text</Text>
+      <Section className="fixed bottom-0 flex gap-2 h-64 mx-4 outline outline-solid outline-neutral-600">
+        <div>Home</div>
+        <div>
+          <a href="https://t.me/ASGKh2Bot/Task">App</a>
+        </div>
+        <div>Team</div>
+        <div>About</div>
+      </Section>
     </AppRoot>
   );
 };
